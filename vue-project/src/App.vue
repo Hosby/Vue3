dfaaf6dc1b17286cdc9e9a242df89096fe8ed43b
김호연 <70309863+Hosby@users.noreply.github.com>
2023-09-19 10:03:20 +0900

    init commit

---
+++ vue-project/src/App.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import ChildComponent from './ChildComponent.vue';
+
+const post = {
+  id: 1,
+  title: 'hosb의 vue 활용',
+  mainCont: 'props와 emit 사용 방법은 ~~',
+}
+
+const receive = (newValue: string) => {
+	console.log('newValue ::', newValue) // 수정한 값이 parent에게 값이 넘어옴
+	post.mainCont = newValue
+}
+</script>
+
+<template>
+  <child-component
+	:title="post.title"
+    :main-cont="post.mainCont"
+	@update:main-cont="receive($event)"
+  />
+</template>
---
+++ vue-project/src/ChildComponent.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+
+const props = defineProps(['title', 'mainCont']) // ParentComponent에서 받은값
+const emits = defineEmits(['update:mainCont'])
+
+const vo = ref('')
+
+onMounted(() => {
+  vo.value = props.mainCont // vo에 받아온 props 값을 매핑함
+})
+
+</script>
+
+<template>
+  <h3> {{ props.title }} </h3>
+  <textarea
+    v-model="vo"
+    @change="emits('update:mainCont', vo)"
+  />
+</template>
